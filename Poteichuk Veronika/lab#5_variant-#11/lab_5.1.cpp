e56660501a4deb94f75cb9cacc6642dd9145b5c1
VeronikaPotseichuk <62996245+VeronikaPotseichuk@users.noreply.github.com>
2020-05-21 12:52:08 +0300

    Лабораторные 1-7
    Лабораторная 4.1 переделана (конструкция if..else заменена на switch..case).

---
+++ Poteichuk Veronika/lab#5_variant-#11/lab_5.1.cpp
@@ -0,0 +1,210 @@
+﻿/*Задана очередь, указывающая порядок обработки одного из n стеков. 
+Элементы очереди содержат номер обрабатываемого стека i (i <= n); признак выполняемого действия: ‘А’ – элемент из очереди добавляется в i - й стек, ‘D’ – из i - го стека удаляется элемент; данные (целые число d).
+Разработать функцию обработки стеков в соответствии с заданной очередью.*/
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+#include <cstdio>
+
+
+typedef struct Stack
+{
+	Stack* next;
+	int data;
+} Stack;
+
+typedef struct Queue
+{
+	Queue* next;
+	Queue* prev;
+	int data;
+	int numOfStack;
+	char action;
+} Queue;
+
+//объявляем функции
+int read();
+void PushStack(Stack** head, int value);
+int PopStack(Stack** head);
+bool IsEmptyStack(Stack** head);
+void PushQueue(Queue** head, int value, char act, int nOS);
+Queue* PopQueue(Queue** head);
+
+//реализуем функции
+int read()
+{
+	char c; //Переменная для хранения каждого введённого символа
+	int num, i; //Переменная - вводимое число
+	char str[9];
+	bool correct;
+	do
+	{
+		i = 0;
+		correct = true;
+		while (i < 8 && (c = _getch()) != 13 && c != ' ') //Проверка на границы и получение символа с клавиатуры
+		{
+			str[i++] = c;
+			_putch(c);
+		}
+		for (int j = 0; j < i; j++)
+		{
+			if (!isdigit(str[j]))
+			{
+				printf("\nTry again\n");
+				correct = false;
+				break;
+			}
+		}
+		if (i == 0) correct = false;
+	} while (correct != true); //Если не число - ввод числа повторяется
+
+	str[i] = '\0'; //Сформированную строку завершаем нуль терминатором
+
+	num = atoi(str); //Преобразуем строку в число
+	return num;
+}
+
+void PushStack(Stack** head, int value)
+{
+	Stack* temp = (Stack*)malloc(sizeof(Stack));
+	if (temp == NULL) printf("STACK_OVERFLOW");
+	if (*head == NULL)
+	{
+		*head = temp;
+		temp->data = value;
+		temp->next = NULL;
+	}
+	else
+	{
+		temp->next = *head;
+		temp->data = value;
+		*head = temp;
+	}
+}
+
+int PopStack(Stack** head)
+{
+	if (*head != NULL)
+	{
+		int result = (*head)->data;
+		*head = (*head)->next;
+		return result;
+	}
+}
+
+bool IsEmptyStack(Stack** head)
+{
+	if (*head == NULL) return true;
+	else return false;
+}
+
+void PushQueue(Queue** head, int value, char act, int nOS)
+{
+	Queue* temp = (Queue*)malloc(sizeof(Queue));
+	if (temp == NULL) printf("QUEUE_OVERFLOW");
+	if (*head == NULL)
+	{
+		*head = temp;
+		temp->data = value;
+		temp->action = act;
+		temp->numOfStack = nOS;
+		temp->next = temp;
+		temp->prev = temp;
+	}
+	else
+	{
+		temp->next = *head;
+		temp->prev = (*head)->prev;
+		(*head)->prev->next = temp;
+		(*head)->prev = temp;
+		temp->data = value;
+		temp->action = act;
+		temp->numOfStack = nOS;
+	}
+}
+
+Queue* PopQueue(Queue** head)
+{
+	if (*head != NULL)
+	{
+		Queue* res = *head;
+		(*head)->prev->next = (*head)->next;
+		(*head)->next->prev = (*head)->prev;
+		*head = (*head)->next;
+		return res;
+	}
+}
+
+int main()
+{
+	int n, numsOfQueue, numOfStack, data, i = 0;
+	char action;
+	printf("Write number of stacks: ");
+	n = read();
+	Stack** heads = (Stack**)calloc(n, sizeof(Stack*));
+	for (int j = 0; j < n; j++)
+	{
+		heads[j] = NULL;
+	}
+	printf("\nWrite number of queue elements: ");
+	numsOfQueue = read();
+	Queue* headQueue = NULL;
+	printf("\nStack || Action (A/D) || Data\n");
+
+	while (i < numsOfQueue)
+	{
+		//считываем данные для элемента очереди
+		numOfStack = read();
+		printf(" ");
+		do
+		{
+			action = _getch();
+			if (action == 'D' || action == 'A')
+			{
+				_putch(action);
+			}
+		} while (action != 'A' && action != 'D');
+		printf(" ");
+		data = read();
+		printf("\n");
+		//проверяем на корректность
+		if (numOfStack > 0 && numOfStack <= n)
+		{
+			PushQueue(&headQueue, data, action, numOfStack);
+			i++;
+		}
+		else
+		{
+			printf("This stack doesn't exist\n");
+		}
+	}
+	//добавляем элементы из очереди в стеки
+	for (int j = 0; j < numsOfQueue; j++)
+	{
+		Queue* temp = PopQueue(&headQueue);
+		numOfStack = temp->numOfStack;
+		action = temp->action;
+		Stack* thisStack = heads[j];
+		if (action == 'A')
+		{
+			PushStack(&heads[numOfStack - 1], temp->data);
+		}
+		else
+		{
+			PopStack(&heads[numOfStack - 1]);
+		}
+	}
+	//выводим информацию о результатах
+	for (int j = 0; j < n; j++)
+	{
+		printf("\n%d stack: ", j + 1);
+		while (!IsEmptyStack(&heads[j]))
+		{
+			printf("%d, ", PopStack(&heads[j]));
+		}
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
---
+++ Poteichuk Veronika/lab#6_variant-#11/lab_5.2.cpp
@@ -0,0 +1,220 @@
+﻿//Разработать функции сравнения и копирования бинарного дерева
+
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+#include <locale>
+#include <cstdio>
+
+typedef struct Node
+{
+	int key;
+	Node* left, * right;
+} Node;
+typedef Node* PNode;
+
+//объявляем функции
+int read();
+bool DoesItEqual(PNode FirstTree, PNode SecondTree);
+PNode CopyFrom(PNode OldTree);
+PNode MakeTree(int data[], int from, int n);
+void ShowTree(PNode Tree, int level, int vertical);
+int* WriteMass(int& number);
+
+//реализуем функции
+int read()
+{
+	char c; //Переменная для хранения каждого введённого символа
+	int num, i; //Переменная - вводимое число
+	char str[9];
+	bool correct;
+	do
+	{
+		i = 0;
+		correct = true;
+		while (i < 8 && (c = _getch()) != 13 && c != ' ') //Проверка на границы и получение символа с клавиатуры
+		{
+			str[i++] = c;
+			_putch(c);
+		}
+		for (int j = 0; j < i; j++)
+		{
+			if (!isdigit(str[j]))
+			{
+				printf("\nTry again\n");
+				correct = false;
+				break;
+			}
+		}
+	} while (correct != true); //Если не число - ввод числа повторяется
+
+	str[i] = '\0'; //Сформированную строку завершаем признаком окончания строки
+
+	num = atoi(str); //Преобразуем строку в число
+	return num;
+}
+
+bool DoesItEqual(PNode FirstTree, PNode SecondTree)
+{
+	bool equal;
+	//если деревья пустые то возвращется true
+	if (FirstTree == NULL && SecondTree == NULL) equal = true;
+	else if (FirstTree != NULL && SecondTree != NULL)
+	{
+		//сравниваем ключи в корнях поддеревьев и рекурсивно вызываем сравнение их левых и правых поддеревьев
+		equal = (FirstTree->key == SecondTree->key && DoesItEqual(FirstTree->left, SecondTree->left) && DoesItEqual(FirstTree->right, SecondTree->right));
+	}
+	//если одно поддерево пустое, а второе нет - возвращается false 
+	else equal = false;
+	return equal;
+}
+
+PNode CopyFrom(PNode OldTree)
+{
+	//создаём узел и копируем данные
+	PNode CloneTree = new Node;
+	CloneTree->key = OldTree->key;
+	//левому и правому поддереву клона рекурсивно присваиваем указатели на клоны левого и правого поддеревьев копируемого дерева
+	if (OldTree->left != NULL) CloneTree->left = CopyFrom(OldTree->left);
+	else CloneTree->left = NULL;
+	if (OldTree->right != NULL) CloneTree->right = CopyFrom(OldTree->right);
+	else CloneTree->right = NULL;
+	return CloneTree;
+}
+
+PNode MakeTree(int data[], int from, int n)
+{
+	//создаем сбалансированное дерево
+	PNode Tree;
+	int n1, n2;
+	if (n == 0) return NULL;
+	Tree = new Node;
+	//ключу корня присваиваем значение среднего элемента массива
+	Tree->key = data[from];
+	n1 = n / 2;
+	n2 = n - n1 - 1;
+	//передаём левому поддереву значения массива до среднего элемента, правому - после
+	Tree->left = MakeTree(data, from + 1, n1);
+	Tree->right = MakeTree(data, from + n1 + 1, n2);
+	return Tree;
+}
+
+void ShowTree(PNode Tree, int level, int vertical)
+{
+	if (Tree != NULL)
+	{
+		int clone = Tree->key;
+		int sizekey = 0;
+		int clonevertical = (vertical - ((vertical >> (level - 1)) << (level - 1))) + (1 << (level - 1));
+		if (level > 1)
+		{
+			while (clonevertical != 1)
+			{
+				printf("          ");
+				if (clonevertical - ((clonevertical >> 1) << 1) == 1)
+				{
+					printf("%c", 166);
+				}
+				else
+				{
+					printf(" ");
+				}
+				clonevertical = clonevertical >> 1;
+			}
+		}
+		if (level != 0)
+		{
+			printf("          %c", 187);
+		}
+		printf("(%d)", Tree->key);
+		while (clone != 0)
+		{
+			clone /= 10;
+			sizekey++;
+		}
+		for (int i = 0; i < 10 - sizekey - 2 && (Tree->left != NULL || Tree->right != NULL); i++)
+		{
+			printf("%c", 151);
+		}
+		if (Tree->left != NULL || Tree->right != NULL)
+		{
+			printf("%c", 172);
+		}
+		printf("\n");
+		ShowTree(Tree->left, level + 1, vertical + (1 << level));
+		ShowTree(Tree->right, level + 1, vertical);
+	}
+}
+
+int* WriteMass(int& number)
+{
+	printf("\nКоличество узлов в дереве (ориентация ЛКП): ");
+	number = read();
+	int* Mass = (int*)calloc(number, sizeof(int));
+	printf("\nВведите числа, содержащиеся в узлах: ");
+	for (int i = 0; i < number; i++)
+	{
+		Mass[i] = read();
+		printf(" ");
+	}
+	return Mass;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int choice, numOfFirst, numOfSecond;
+	int* FirstMass, * SecondMass;
+	PNode FirstTree = NULL, SecondTree = NULL;
+	do
+	{
+		system("cls");
+		printf("1. Ввести два дерева");
+		printf("\n2. Сравнить деревья");
+		printf("\n3. Копировать первое дерево во второе");
+		printf("\n4. Копировать второе дерево в первое");
+		printf("\n5. Вывести деревья");
+		printf("\n6. Выход");
+		printf("\nВаш выбор: ");
+		choice = read();
+		switch (choice)
+		{
+		case 1:
+			system("cls");
+			printf("Первое дерево:");
+			FirstMass = WriteMass(numOfFirst);
+			FirstTree = MakeTree(FirstMass, 0, numOfFirst);
+			printf("\nВторое дерево:");
+			SecondMass = WriteMass(numOfSecond);
+			SecondTree = MakeTree(SecondMass, 0, numOfSecond);
+			break;
+		case 2:
+			system("cls");
+			if (DoesItEqual(FirstTree, SecondTree)) printf("\nДеревья равны.");
+			else printf("Деревья не равны.\n");
+			system("pause");
+			break;
+		case 3:
+			system("cls");
+			SecondTree = CopyFrom(FirstTree);
+			printf("OK\n");
+			system("pause");
+			break;
+		case 4:
+			system("cls");
+			FirstTree = CopyFrom(SecondTree);
+			printf("\nOK\n");
+			system("pause");
+			break;
+		case 5:
+			system("cls");
+			printf("Первое дерево:\n\n");
+			ShowTree(FirstTree, 0, 0);
+			printf("\nВторое дерево:\n\n");
+			ShowTree(SecondTree, 0, 0);
+			system("pause");
+		}
+	} while (choice != 6);
+	return 0;
+}
+
